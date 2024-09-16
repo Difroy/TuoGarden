66bd3a3bde818a5b495abf6161e79fdad7220d2c
Difroy <unifroy@gmail.com>
2024-09-16 11:52:53 +0200

    Update PlantRepository.java

--- src/main/java/com/generation/garden/model/repository/PlantRepository.java
+++ src/main/java/com/generation/garden/model/repository/PlantRepository.java
@@ -1,5 +1,7 @@
 package com.generation.garden.model.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.generation.garden.model.entities.Plant;
@@ -7,7 +9,9 @@ import com.generation.garden.model.entities.Plant;
 public interface PlantRepository extends JpaRepository <Plant, Long>{
 
 	
+	List<Plant> findByStatus(String status);
 	
+	List<Plant> findByNameContaining (String n);
 	
 	
 	
