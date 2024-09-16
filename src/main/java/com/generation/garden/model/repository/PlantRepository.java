aba7b2fc41e2bc1b16692c3847186c9c0882fb40
Difroy <unifroy@gmail.com>
2024-09-16 10:20:55 +0200

    Create PlantRepository.java

---
+++ src/main/java/com/generation/garden/model/repository/PlantRepository.java
@@ -0,0 +1,14 @@
+package com.generation.garden.model.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.generation.garden.model.entities.Plant;
+
+public interface PlantRepository extends JpaRepository <Plant, Long>{
+
+	
+	
+	
+	
+	
+}
